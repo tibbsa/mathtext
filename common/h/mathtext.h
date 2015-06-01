74d1c1a52c2c2030b08b1ba1e084eefb5c636bb6
Anthony Tibbs <anthony@tibbs.ca>
2015-06-01 08:49:42 -0400

    Version numbering and makefile work

---
+++ common/h/mathtext.h
@@ -0,0 +1,18 @@
+/**
+ * @file mathtext.h
+ * General MathText header file
+ * 
+ * @copyright Copyright 2015 Anthony Tibbs
+ * This project is released under the GNU General Public License.
+*/
+
+#ifndef __MATHTEXT_H__
+#define __MATHTEXT_H__
+
+/**
+ * Version number and tagline strings
+ */
+#define MATHTEXT_VERSION_NUMBER "0.10"
+#define MATHTEXT_VERSION_TAGLINE "Startin' Fresh"
+
+#endif /* __MATHTEXT_H__ */
--- linux/main.cpp
+++ linux/main.cpp
@@ -1,33 +1,32 @@
-/*
-**
-** linux/main.cpp: Command-line interface for Mathext
-** Copyright (C) 2015 Anthony Tibbs
-**
-** This program is free software: you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation, either version 3 of the License, or
-** at your option) any later version.
-** 
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-** GNU General Public License for more details.
-** 
-** You should have received a copy of the GNU General Public License
-** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * @file main.cpp
+ * Command-line interface for MathText on Linux.
+ * 
+ * @copyright Copyright 2015 Anthony Tibbs
+ * This project is released under the GNU General Public License.
 */
+
+using namespace std;
+
 #include <iostream>
 
-/**
+#include "mathtext.h"
+
+/** 
  * Parses the command line and launches the necessary conversions.
  *
- * @param argc     Number of argments on command line
- * @param argv     Array of c-strings containing command line arguments
+ * @param argc Number of argments on command line
+ * @param argv Array of c-strings containing command line arguments
  * @return An integer: 0 on success, 1 for translations errors, 2 for 
  *         other errors
  */
 int main (const int argc, const char **argv)
 {
-  
+  cout << "MathText " << MATHTEXT_VERSION_NUMBER ", build " <<
+    MATHTEXT_VERSION_BUILD << " - " << MATHTEXT_VERSION_TAGLINE << endl;
+  cout << "Copyright (C) 2015 Anthony Tibbs" << endl << endl;
+  cout << "This program is FREE SOFTWARE and comes with ABSOLUTELY NO WARRANTY." << endl;
+  cout << "See LICNESE for more information." << endl << endl;
+
   return 0;
 }
