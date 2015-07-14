c4cb60e38a8cf86076a1eec543c3aed61b7964c2
Anthony Tibbs <anthony@tibbs.ca>
2015-07-14 13:39:17 -0400

    Added CATCH unit testing framework and mttest application core

---
+++ linux/mttest.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file mttest.cpp
+ * Unit testing driver (to run on Linux)
+ * 
+ * @copyright Copyright 2015 Anthony Tibbs
+ * This project is released under the GNU General Public License.
+*/
+
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
