fe1b6890b7ad3bac3e208d2347ed7e8964752423
Anthony Tibbs <anthony@tibbs.ca>
2015-07-15 12:24:20 -0400

    Worked on numeric mode UEB tests

--- test/rendering/ueb.cpp
+++ test/rendering/ueb.cpp
@@ -24,6 +24,10 @@
 TEST_CASE("render/ueb/numbers", "[render][ueb]") {
   UEBRenderer r;
 
+  // UEB Rulebook 6.1 ----------------------------------------------
+  SECTION ("number mapping is correct") { X("0123456789", "", "#JABCDEFGHI") }
+
+  // Examples: 100, -100, 1.25, -1.25, .25, -.25
   SECTION ("positive integers") { X("100", "", "#AJJ") }
   SECTION ("negative integers") { X("-100", "", "-#AJJ") }
   SECTION ("positive decmial + leading digits") { X("1", "25", "#A4BE") }
@@ -31,25 +35,73 @@ TEST_CASE("render/ueb/numbers", "[render][ueb]") {
   SECTION ("positive decmial w/o leading digits") { X("", "25", "#4BE") }
   SECTION ("negative decmial w/o leading digits") { X("-", "25", "-#4BE") }
 
-  SECTION ("letter indicators for a-j when appearing after numbers") {
-    for (char c = 'a'; c <= 'j'; c++) {
-      MDEVector v;
-      v.push_back (boost::make_shared<MDE_Number>(S("10"), S("")));
-      v.push_back (boost::make_shared<MDE_MathBlock>(CHARSTR(c)));
-      
-      std::string output = r.renderVector(v);
-      REQUIRE (output == S("#AJ;" + CHARSTR(c)));
+  // UEB Rulebook 6.5 ----------------------------------------------
+  // Scenario 1: Letter follows number
+  //
+  SECTION ("where a number ends in a digit") {
+    MDEVector v;
+    v.push_back (boost::make_shared<MDE_Number>(S("10"), S("")));
+
+    // Example: 10a
+    SECTION ("and a-j follow, expect letter indicator") {
+      for (char c = 'a'; c <= 'j'; c++) {
+	v.push_back (boost::make_shared<MDE_MathBlock>(CHARSTR(c)));
+	REQUIRE (r.renderVector(v) == S("#AJ;" + CHARSTR(c)));
+	v.pop_back();
+      }
+    }
+
+    // Example: 10k
+    SECTION ("and k-z follow, no letter indicator required") {
+      for (char c = 'k'; c <= 'z'; c++) {
+	v.push_back (boost::make_shared<MDE_MathBlock>(CHARSTR(c)));
+	REQUIRE (r.renderVector(v) == S("#AJ" + CHARSTR(c)));
+	v.pop_back();
+      }
+    }
+
+    // Example: 10C
+    SECTION ("and A-Z (capitals) follow, no letter indicator required") {
+      for (char c = 'A'; c <= 'Z'; c++) {
+	v.push_back (boost::make_shared<MDE_MathBlock>(CHARSTR(c)));
+	REQUIRE (r.renderVector(v) == S("#AJ," + CHARSTR(c)));
+	v.pop_back();
+      }
     }
   }
+    
+  // Scenario 2: Letter follows a period at the end of a number
+  //
+  SECTION ("where a number ends in a period") {
+    MDEVector v;
+    v.push_back (boost::make_shared<MDE_Number>(S("10"), S("")));
+    v.push_back (boost::make_shared<MDE_MathBlock>(S(".")));
 
-  SECTION ("no letter indicators for k-z when appearing after numbers") {
-    for (char c = 'k'; c <= 'z'; c++) {
-      MDEVector v;
-      v.push_back (boost::make_shared<MDE_Number>(S("10"), S("")));
-      v.push_back (boost::make_shared<MDE_MathBlock>(CHARSTR(c)));
-      
-      std::string output = r.renderVector(v);
-      REQUIRE (output == S("#AJ" + CHARSTR(c)));
+    // Example: 10.a
+    SECTION ("and a-j follow, expect letter indicator") {
+      for (char c = 'a'; c <= 'j'; c++) {
+	v.push_back (boost::make_shared<MDE_MathBlock>(CHARSTR(c)));
+	REQUIRE (r.renderVector(v) == S("#AJ4;" + CHARSTR(c)));
+	v.pop_back();
+      }
+    }
+    
+    // Example: 10.k
+    SECTION ("and k-z follow, no letter indicator required") {
+      for (char c = 'k'; c <= 'z'; c++) {
+	v.push_back (boost::make_shared<MDE_MathBlock>(CHARSTR(c)));
+	REQUIRE (r.renderVector(v) == S("#AJ4" + CHARSTR(c)));
+	v.pop_back();
+      }
+    }
+
+    // Example: 10.M
+    SECTION ("and A-Z (capitals) follow, no letter indicator required") {
+      for (char c = 'A'; c <= 'Z'; c++) {
+	v.push_back (boost::make_shared<MDE_MathBlock>(CHARSTR(c)));
+	REQUIRE (r.renderVector(v) == S("#AJ," + CHARSTR(c)));
+	v.pop_back();
+      }
     }
   }
 }
